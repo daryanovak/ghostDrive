1cc47a86ef139314cd200dc48e74632aa6fbf83f
Darya Novak <daryanovak14@gmail.com>
2018-12-12 02:34:13 +0300

    Fix upload, button layout, shared list row click, readonly details empty tags.

--- GhostDrive.Web/wwwroot/js/files.js
+++ GhostDrive.Web/wwwroot/js/files.js
@@ -1,10 +1,15 @@
 ﻿$(document).ready(function() {
     var myDropzone = Dropzone.forElement("#UploadForm");
+    Dropzone.options.myDropzone = {
+        parallelUploads: 1
+    };
     myDropzone.on("complete",
         function () {
-            var actionPathPart = "Files/Index";
-            var index = location.href.indexOf(actionPathPart);
-            location.href = location.href.substring(0, index + actionPathPart.length);
+            if (this.getUploadingFiles().length === 0 && this.getQueuedFiles().length === 0) {
+                var actionPathPart = "Files/Index";
+                var index = location.href.indexOf(actionPathPart);
+                location.href = location.href.substring(0, index + actionPathPart.length);
+            }
         });
     try {
         TagCanvas.textColour = "#ff0000";
