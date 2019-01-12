b5a5a46da546f630f76873dda06a2acd3651098d
Darya Novak <daryanovak14@gmail.com>
2019-01-12 19:04:54 +0300

    ShortLinkServiceTests and FileTests

---
+++ GhostDrive.Application.Tests/Services/ShortLinkServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using GhostDrive.Infrastructure.Services;
+using Xunit;
+
+namespace GhostDrive.Application.Tests.Services
+{
+    public class ShortLinkServiceTests
+    {
+        [Theory]
+        [InlineData("https:///localhost", true)]
+        [InlineData("https://localhost/Files/Details", false)]
+        public async Task GetShortLink(string longLink, bool isNull)
+        {
+            var subject = new ShortLinkService();
+
+            var result = await subject.GetShortLink(longLink);
+
+            Assert.True(isNull && result == null || !isNull && result != null);
+        }
+    }
+}
---
+++ GhostDrive.Domain.Tests/Models/FileTests.cs
@@ -0,0 +1,20 @@
+﻿using GhostDrive.Domain.Models;
+using Xunit;
+
+namespace GhostDrive.Domain.Tests.Models
+{
+    public class FileTests
+    {
+        [Fact]
+        public void ShouldHaveCorrectFullName()
+        {
+            var file = new File
+            {
+                Name = "Name",
+                Extension = "txt"
+            };
+
+            Assert.Equal("Name.txt", file.FullName);
+        }
+    }
+}
